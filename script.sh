673528356f014d91ffa3a54eebd755678395be31
Sagar Shah <sagarshah2326@gmail.com>
2019-02-13 07:03:14 +0100

    Add

---
+++ script.sh
@@ -0,0 +1,20 @@
+wget https://ss2.oboom.com/1/dlh?ticket=5fe001b0-e082-4421-bbea-d149200f5894
+mv dlh* part5.rar
+wget https://ss13.oboom.com/1/dlh?ticket=3f6c9e9f-9868-43fd-b067-fb32b4979b71
+mv dlh* part6.rar
+wget https://ss5.oboom.com/1/dlh?ticket=080fd22c-70e5-421d-bb1c-ab02d426afc9
+mv dlh* part7.rar
+wget https://ss11.oboom.com/1/dlh?ticket=bc3a8a57-2ef7-4eae-bb12-fdd6227fdcc6
+mv dlh* part8.rar
+wget https://ss9.oboom.com/1/dlh?ticket=ffed4a28-2a67-496c-a38a-f5d2cc0b94bb
+mv dlh* part9.rar
+wget https://ss13.oboom.com/1/dlh?ticket=c705065b-0e01-46fb-b23a-282ef4f8784e
+mv dlh* part10.rar
+wget https://ss11.oboom.com/1/dlh?ticket=c9a40958-fe4f-470f-aba8-b8be9afca68f
+mv dlh* part11.rar
+wget https://ss2.oboom.com/1/dlh?ticket=9dfc3970-31f9-44fe-bde8-9c8f120d4ed2
+mv dlh* part12.rar
+wget https://ss11.oboom.com/1/dlh?ticket=393d0a19-9059-4f58-b466-560770e1caf5
+mv dlh* part13.rar
+wget https://ss5.oboom.com/1/dlh?ticket=5d94b516-d234-46c2-8316-7b4827a71e3e
+mv dlh* part14.rar
